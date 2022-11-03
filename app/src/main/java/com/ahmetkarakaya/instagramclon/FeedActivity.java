c55e0737bf48208250017d21262b19d243fec9e1
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-11-03 20:44:28 +0300

    Çıkış işlemi tamamlandı.

--- app/src/main/java/com/ahmetkarakaya/instagramclon/FeedActivity.java
+++ app/src/main/java/com/ahmetkarakaya/instagramclon/FeedActivity.java
@@ -1,14 +1,51 @@
 package com.ahmetkarakaya.instagramclon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FeedActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add_post){
+            //upload aktivity
+            Intent intent = new Intent(FeedActivity.this, UploadActivity.class);
+            startActivity(intent);
+        }
+        else if(item.getItemId() == R.id.signout){
+            //signout
+            auth.signOut();
+
+            Intent intent = new Intent(FeedActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
