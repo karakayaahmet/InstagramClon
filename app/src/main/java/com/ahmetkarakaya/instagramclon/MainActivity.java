0ec708f484d9b096e5cc33ab702496e0b55d30d9
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-11-03 20:14:59 +0300

    Kayıt işlemleri tamamlandı.

--- app/src/main/java/com/ahmetkarakaya/instagramclon/MainActivity.java
+++ app/src/main/java/com/ahmetkarakaya/instagramclon/MainActivity.java
@@ -1,16 +1,35 @@
 package com.ahmetkarakaya.instagramclon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.ahmetkarakaya.instagramclon.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
+    private FirebaseAuth auth;
+    String email;
+    String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        auth = FirebaseAuth.getInstance();
+
+        email = binding.emailText.getText().toString();
+        password = binding.passwordText.getText().toString();
     }
 
     public void signin(View view){
@@ -18,6 +37,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signup(View view){
+        email = binding.emailText.getText().toString();
+        password = binding.passwordText.getText().toString();
+        if (email.equals("") || password.equals("")){
+            Toast.makeText(this, "Enter email and password", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
 
     }
 }
