51d01e5c94e22a84ce6c25b6c9c70cbe6995c933
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-11-04 08:05:59 +0300

    İzinler oluşturuldu.

--- app/src/main/java/com/ahmetkarakaya/instagramclon/UploadActivity.java
+++ app/src/main/java/com/ahmetkarakaya/instagramclon/UploadActivity.java
@@ -1,16 +1,40 @@
 package com.ahmetkarakaya.instagramclon;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 
+import com.ahmetkarakaya.instagramclon.databinding.ActivityUploadBinding;
+import com.google.android.material.snackbar.Snackbar;
+
 public class UploadActivity extends AppCompatActivity {
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
+    Uri imageData;
+    private ActivityUploadBinding binding;
+    Bitmap selectedBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload);
+        binding = ActivityUploadBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
     public void share(View view){
@@ -18,6 +42,53 @@ public class UploadActivity extends AppCompatActivity {
     }
 
     public void select_image(View view){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view, "Permission needen for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //ask permission
+                    }
+                }).show();
+            }
+            else{
+                //ask permission
+            }
+        }
+        else{
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        }
+    }
 
+    private void registerLauncher(){
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK){
+                    Intent intent = result.getData();
+
+                    if (intent != null){
+                        imageData = intent.getData();
+                        binding.imageView.setImageURI(imageData);
+
+                       /*
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28){
+                                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), imageData);
+                                selectedBitmap = ImageDecoder.decodeBitmap(source);
+                                binding.imageView.setImageBitmap(selectedBitmap);
+                            }
+                            else{
+                                selectedBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageData);
+                                binding.imageView.setImageBitmap(selectedBitmap);
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        } */
+                    }
+                }
+            }
+        });
     }
 }
